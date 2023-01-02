3e195409d87f89723a0634912d479644a6942800
dharan1999 <dharanthakar96@gmail.com>
2023-01-02 17:36:16 -0500

    Mongodb connection established

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const mongoURI = 'mongodb://localhost:27017/'
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI, () => {
+        console.log("Connected to the mongodb database successfully");
+    })
+}
+module.exports = connectToMongo;
+mongoose.set('strictQuery', false);
---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const connectToMongo = require('./db');
+const express = require('express')
+const app = express()
+connectToMongo();
+
+
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
