c00107b7d723aa67e1c0378639a1d6a05f2c8470
dharan1999 <dharanthakar96@gmail.com>
2023-01-04 16:08:30 -0500

    models and routes for users and notes created

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,15 @@ const express = require('express')
 const app = express()
 connectToMongo();
 
-
-
+// If we want to use req.body, then we have to use middleware and to use the middleware we 
+// have to use the app.use(express.json())
+app.use(express.json())
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
---
+++ backend/routes/auth.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const User = require("../models/User");
+const router = express.Router();
+
+
+
+//Create a User with POST "/api/auth". Doesn't require auth1
+
+router.post('/',(req,res) => {
+    console.log(req.body);
+    const user = User(req.body);
+    user.save()
+    res.send(req.body);
+})
+
+
+module.exports = router
